a60f7f63e867b4de3040015bbdbd962ddf381818
Gabriel Machado <GABRIEL.MACHADO-BANDEIRA@SONICDRIVEIN.COM>
2018-11-10 11:09:41 -0300

    first jenkis and terraform configurations

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,40 +16,40 @@ module "network" {
   ips_allowed = ["189.3.135.146/32"]
 }
 
-module "spark_facilitador" {
-  source = "./spark"
-  project = "DE Training Recife"
-  environment = "Sandbox"
-  name = "facilitador"
-  vpc_id = "${module.network.vpc_id}"
-  subnet_id = "${module.network.subnet_id}"
-  configurations = ""
-  key_name = "de-training-recife"
-  bootstrap_name = "null"
-  bootstrap_uri = "null"
-  log_uri = "s3://com.thoughtworks.training.de.recife/facilitador/logs"
-
-  sg_emr_master_id = "${module.network.sg_emr_master_id}"
-  sg_emr_slave_id = "${module.network.sg_emr_slave_id}"
-  emr_ec2_instance_profile_arn = "${module.iam.emr_ec2_instance_profile_arn}"
-  emr_service_role_arn = "${module.iam.emr_service_role_arn}"
-}
-
-//module "spark_par_01" {
+//module "spark_facilitador" {
 //  source = "./spark"
 //  project = "DE Training Recife"
 //  environment = "Sandbox"
-//  name = "par_01"
+//  name = "facilitador"
 //  vpc_id = "${module.network.vpc_id}"
 //  subnet_id = "${module.network.subnet_id}"
 //  configurations = ""
 //  key_name = "de-training-recife"
 //  bootstrap_name = "null"
 //  bootstrap_uri = "null"
-//  log_uri = "s3://com.thoughtworks.training.de.recife/par_01/logs"
+//  log_uri = "s3://com.thoughtworks.training.de.recife/facilitador/logs"
 //
 //  sg_emr_master_id = "${module.network.sg_emr_master_id}"
 //  sg_emr_slave_id = "${module.network.sg_emr_slave_id}"
 //  emr_ec2_instance_profile_arn = "${module.iam.emr_ec2_instance_profile_arn}"
 //  emr_service_role_arn = "${module.iam.emr_service_role_arn}"
 //}
+
+module "spark_par_05" {
+  source = "./spark"
+  project = "DE Training Recife"
+  environment = "Sandbox"
+  name = "par_05"
+  vpc_id = "${module.network.vpc_id}"
+  subnet_id = "${module.network.subnet_id}"
+  configurations = ""
+  key_name = "de-training-recife"
+  bootstrap_name = "null"
+  bootstrap_uri = "null"
+  log_uri = "s3://com.thoughtworks.training.de.recife/par_05/logs"
+
+  sg_emr_master_id = "${module.network.sg_emr_master_id}"
+  sg_emr_slave_id = "${module.network.sg_emr_slave_id}"
+  emr_ec2_instance_profile_arn = "${module.iam.emr_ec2_instance_profile_arn}"
+  emr_service_role_arn = "${module.iam.emr_service_role_arn}"
+}
